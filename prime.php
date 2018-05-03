345b9e6f54debb02807f0fca3ccbfdb450f645f8
Keyrainn <dzakirin4@gmail.com>
2018-05-03 15:40:51 +0800

    Update function

--- prime.php
+++ prime.php
@@ -1,12 +1,10 @@
 <?php
-
 class Prime
 {
     private $pNo = 0;
-
+    private $count = 0;
    public function checkPrimeNo($num){      
       for($i=1; $i <= $num; $i++){
-          $count = 0;
           $counter = 0; 
           for($j=1; $j <= $i; $j++){ 
                 if($i % $j==0){ 
@@ -14,35 +12,16 @@ class Prime
                 }
           }
         // Display prime number
-        if($counter==2){ 
-         //   echo $i ." Prime number </br>";
+        if($counter==2){
+            //echo $i ." Prime number </br>";
             $this->pNo += $i;
+            $this->count++; 
+            
             for($k=$this->pNo; --$k && $this->pNo %$k;);
             if ($k==1)
-            echo $this->pNo. "</br>";
-
+            echo "Sum of " . $this->count . " term is : " . $this->pNo . "</br>";
           }
         }
     }
-
-    public function sumPrime(){
-        $bCheck = True; 
-                for ($k = 2; $k < $this->pNo; $k++)
-                {
-                    if ($this->pNo % $k == 0) 
-                    {
-                        echo $this->pNo . 'Prime sum' ;  
-                        break;
-                    }               
-                }
-            if ($bCheck) 
-                echo $this->pNo . 'Prime sum' ;           
-            else
-                echo '</br>'; 
-    }
 }
-
-
- //  if ($this->pNo += $i % 0) {      echo $this->pNo;      }
 ?>
-

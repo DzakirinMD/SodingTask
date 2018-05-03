123f6e320c81e6c1c8b5d5fe698acf2f8014178f
Keyrainn <dzakirin4@gmail.com>
2018-05-04 01:21:55 +0800

    Added new algorithm

--- index.php
+++ index.php
@@ -1,9 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Soding Task</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
 
-include 'Prime.php';
+    <form method="post" action="">
+    <input type="text" name="value">
+    <input type="submit">
+    </form>
+    <?php
+    include 'prime.php';
 
-$p1 = new Prime;
+    $p = new IsPrime();
+    $max_sum = $_POST['value'];
+    $sum = 0;
+    $num = 6;
+    $term = 0;
 
-echo $p1->checkPrimeNo(1000);
-
-?>
+        while (($sum + $num) < $max_sum) {
+            if ($p->check($num)) {
+                $sum += $num;
+                $term++; // track term
+            }
+            $num++;
+        }
+        echo "The sum below ". $max_sum . " is : " .$sum;
+        echo "</br>Consisting of : " . $term . " terms";
+    ?>
+</body>
+</html>
--- prime.php
+++ prime.php
@@ -1,26 +1,27 @@
 <?php
-class Prime
-{
-    private $pNo = 0;
-    private $count = 0;
-   public function checkPrimeNo($num){      
-      for($i=1; $i <= $num; $i++){
-          $counter = 0; 
-          for($j=1; $j <= $i; $j++){ 
-                if($i % $j==0){ 
-                    $counter++;
-                }
-          }
-        if($counter==2){
-            //echo $i ." is a Prime number </br>";
-            $this->pNo += $i;
-            $this->count++; 
-            
-            for($k=$this->pNo; --$k && $this->pNo %$k;);
-            if ($k==1)
-            echo "Sum of " . $this->count . " term is : " . $this->pNo . "</br>";
-          }
+class IsPrime
+{       
+     function check($num)
+     {
+        $bCheck = True; 
+        for ($i = 2; $i < $num; $i++)
+        {
+            if ($num % $i == 0) 
+            {
+                 $bCheck = False;
+                 break;
+            }               
         }
-    }
-}
+       if ($bCheck) 
+          //echo 'Prime';    
+          return true;       
+       else
+          //echo 'NOT prime'; 
+          return false;
+     }       
+  }
+
+ 
 ?>
+
+      
